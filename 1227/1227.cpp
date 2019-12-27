212bc48c206565aa46706b9df6a3ded4d7643cf7
help4cura <kitsune950907@gmail.com>
2019-12-27 16:50:00 +0900

    1227 과제

---
+++ 1227/1227.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+float average(float a, float b) 
+{
+    float res;
+    res = a / b;
+
+    return res;
+}
+
+int main()
+{
+    float a, b;
+    cin >> a;
+    cin >> b;
+    
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a << " / " << b << " = " << average(a,b) << endl << endl;
+
+    Sleep(1000);
+    cout << "1" << endl;
+    Sleep(1000);
+    cout << "2" << endl;
+    Sleep(1000);
+    cout << "3" << endl;
+    Sleep(1000);
+    cout << "4" << endl;
+    Sleep(1000);
+    cout << "5";
+
+    system("cls");
+
+    return 0;
+}
